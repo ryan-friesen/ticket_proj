a76b4a095d4d9c0ee525fe10d12d8c58e759582c
Neil M <neilm813@gmail.com>
2020-01-05 18:19:53 -0800

    create user & display all users (testing)

--- server/ticket_app/models.py
+++ server/ticket_app/models.py
@@ -12,3 +12,9 @@ class User(models.Model):
         validators=[MinLengthValidator(12)], max_length=60)
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
+
+    def __repr__(self):
+        return f"{self.first_name} {self.last_name} {self.email}"
+
+    def __str__(self):
+        return f"{self.first_name} {self.last_name} {self.email}"
--- server/ticket_app/views.py
+++ server/ticket_app/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render, redirect
+from .models import User
 
 
 def index(request):
@@ -6,8 +7,19 @@ def index(request):
 
 
 def register(request):
+    new_user = User.objects.create(
+        first_name=request.POST['first_name'],
+        last_name=request.POST['last_name'],
+        email=request.POST['email'],
+        password=request.POST['password'],
+
+    )
+    print(new_user)
     return redirect("/users/dashboard")
 
 
 def dashboard(request):
-    return render(request, 'ticket_app/dashboard.html')
+    context = {
+        "all_users": User.objects.all()
+    }
+    return render(request, 'ticket_app/dashboard.html', context)
