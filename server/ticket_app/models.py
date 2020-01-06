23db4d853abf398e51fb6725264adf448608acbe
Neil M <neilm813@gmail.com>
2020-01-05 17:53:43 -0800

    Added user model, migrated

--- server/ticket_app/models.py
+++ server/ticket_app/models.py
@@ -1,3 +1,14 @@
 from django.db import models
+from django.core.validators import MinLengthValidator
 
 # Create your models here.
+
+
+class User(models.Model):
+    first_name = models.CharField(max_length=60)
+    last_name = models.CharField(max_length=60)
+    email = models.CharField(max_length=320)
+    password = models.CharField(
+        validators=[MinLengthValidator(12)], max_length=60)
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
